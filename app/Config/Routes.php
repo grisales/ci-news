862f2e95cd3539afc0dea6093c2d40bf8def1bd8
grisales <grisales@gmail.com>
2022-07-16 16:36:53 -0300

    Display the News

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -43,6 +43,8 @@ $routes->set404Override();
 // path using the wildcard string (:any), and passes the parameter to
 // the view() method of the Pages class.
 $routes->get('/', 'Home::index');
+$routes->get('news/(:segment)', 'News::view/$1');
+$routes->get('news', 'News::index');
 $routes->get('pages', 'Pages::index');
 $routes->get('(:any)', 'Pages::view/$1');
 
